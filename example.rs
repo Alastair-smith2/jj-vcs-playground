157f9cfd4923a1be5fe12eb8ac47725c12626050
Alastair Smith <Alastair-smith2@users.noreply.github.com>
2025-02-08 12:02:21 +0000

    Some other work from here
    Some further work

--- example.rs
+++ example.rs
@@ -1,3 +1,10 @@
+#[derive(Debug)]
+struct Example {
+    fieldOne: bool,
+    fieldTwo: String,
+    fieldThree: u32
+}
+
 fn main() {
     let val = 5;
     println!("Hello alpha");
@@ -6,4 +13,7 @@ fn main() {
     let another_value = 20;
     let val = 20;
     let test = 25;
+    let example = Example {fieldOne: false, fieldTwo: String::from("Hey"), fieldThree: 5}
+    println!("No formatter makes the syntax check harder :) {:?}", example);
+    
 }
